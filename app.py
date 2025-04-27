d57d10bd6d4aedc02a2f06fcc3b237825dea285b
Hassan <hhd17@mail.aub.edu>
2025-04-28 00:31:45 +0300

    Preparing for render deployment

--- app.py
+++ app.py
@@ -349,4 +349,4 @@ def decrypt_file():
 
 # Start the Flask app on localhost:5000
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
